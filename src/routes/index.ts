b0fd41fc61b872599ea8fc251af065f4cd0481ca
Ritik <shrestharitik@gmail.com>
2024-07-09 13:31:41 +0545

    removed redirect

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,5 @@ import express from "express";
 import projectRouter from "./todos";
 const router = express();
 router.use("/projects", projectRouter);
-router.use("/", (req, res) => {
-  res.redirect("/projects");
-});
+
 export default router;
