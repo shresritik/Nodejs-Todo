ab6815d242f76739d29babd7f3fad8fdf9616304
Ritik <shrestharitik@gmail.com>
2024-07-15 18:17:35 +0545

    updated yaml file

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 DOCKER_REPO="shresritik/todo"
 
 # Fetch the latest commit hash
-latest_commit_hash=$(git rev-parse --short HEAD)
+latest_commit_hash=b09510d
 # Ensure the .env file exists
 ENV_FILE=".env"
 if [ ! -f "$ENV_FILE" ]; then
