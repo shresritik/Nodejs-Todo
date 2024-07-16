369e6f2a1f505505e6acbc822c86da3d075b5bc8
Ritik <shrestharitik@gmail.com>
2024-07-17 00:22:07 +0545

    updated for docker

--- script.sh
+++ script.sh
@@ -22,5 +22,5 @@ else
     exit 1
 fi
 # Pull the Docker image tagged with the latest commit hash
-docker pull ${DOCKER_REPO}:5.${latest_commit_hash}
-docker run -p ${HOST_PORT}:${HOST_PORT} --env-file=${ENV_FILE} ${DOCKER_REPO}:6.${latest_commit_hash}
+docker pull ${DOCKER_REPO}:7.${latest_commit_hash}
+docker run -p ${HOST_PORT}:${HOST_PORT} --env-file=${ENV_FILE} ${DOCKER_REPO}:7.${latest_commit_hash}
